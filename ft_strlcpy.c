7c901372c4508fd6d33fb492d9bf85d26242a6e3
nmarmugi <154420752+nmarmugi@users.noreply.github.com>
2023-12-20 11:55:09 +0100

    Add files via upload

---
+++ ft_strlcpy.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmarmugi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 11:11:54 by nmarmugi          #+#    #+#             */
+/*   Updated: 2023/12/20 11:12:00 by nmarmugi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && in < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    size_t lunghezza;
+
+    lunghezza = strlen(src, size - 1);
+    if (size == 0)
+        return (0);
+    if (lunghezza >= size -1)
+        return (NULL);
+    ft_strncpy(dst, src, lunghezza);
+    dst[lunghezza] = '\0';
+    return (lunghezza);
+}
