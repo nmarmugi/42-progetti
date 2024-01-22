90e078f80ad84238789adf55655e51722ed8c56e
Nicola Marmugi <nmarmugi@c2r2p3.42firenze.it>
2024-01-22 17:02:00 +0100

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmarmugi <nmarmugi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 11:05:24 by nmarmugi          #+#    #+#             */
+/*   Updated: 2024/01/22 11:59:30 by nmarmugi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
+
+
+
+#endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmarmugi <nmarmugi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 11:52:24 by nmarmugi          #+#    #+#             */
+/*   Updated: 2024/01/22 14:51:55 by nmarmugi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while(s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+	
+	str = (char *)s;
+	c = '\n';
+	while(*str != (char)c)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return ((char *)str);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*result;
+	
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	result = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	while (*s1)
+		*result++ = *s1++;
+	while (*s2)
+		*result++ = *s2++;
+	*result = '\0';
+	return (result - len_s1 - len_s2);
+}
+
+static void	ft_bzero(void *s, size_t n)
+{
+	char	*ptr;
+	size_t	i;
+
+	ptr = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	tot_size;
+	void	*ptr;
+	size_t	i;
+
+	i = 0;
+	tot_size = nmemb * size;
+	ptr = malloc(tot_size);
+	if (nmemb != 0 && tot_size / nmemb != size)
+		return (NULL);
+	if (size > 0)
+		ft_bzero(ptr, tot_size);
+	return (ptr);
+}
