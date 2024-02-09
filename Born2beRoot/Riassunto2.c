4041899524f40c869a543018a5f4370e68344983
Nicola Marmugi <marmu1@hotmail.it>
2024-02-09 16:00:43 +0100

    B2bR

---
+++ Born2beRoot/Riassunto2.c
@@ -0,0 +1,26 @@
+Macchine Virtuali (VM):
+Le macchine virtuali sono software che simulano un ambiente isolato e virtuale su un host fisico, consentendo l'esecuzione di sistemi operativi e applicazioni. Offrono vantaggi come la gestione facilitata, l'esecuzione di sistemi operativi multipli sullo stesso hardware e la sicurezza.
+
+Logical Volume Manager (LVM):
+Il LVM è uno strumento di gestione dello spazio su disco per sistemi operativi basati su Linux. Permette la creazione di pool di archiviazione, chiamati gruppi di volumi, che possono essere suddivisi in volumi logici. Offre flessibilità, snapshot istantanei, migrazione dei dati e semplifica la gestione dello spazio su disco.
+
+Differenze tra Rocky e Debian:
+Rocky è una distribuzione Linux più recente basata su RHEL, orientata alle imprese. Debian è più antica, stabile, flessibile e diffusa. Rocky è più complesso, supporta compilatori recenti, mentre Debian è noto per la vasta repository di pacchetti e l'interfaccia grafica user-friendly.
+
+Apt e Aptitude:
+Apt e Aptitude sono gestori di pacchetti per Debian. Apt è il comando base, più recente e user-friendly, senza interfaccia grafica. Aptitude offre funzionalità più dettagliate, con la possibilità di gestire pacchetti tramite interfaccia grafica nel terminale.
+
+APPArmor:
+APPArmor è un sistema di controllo degli accessi basato su kernel Linux. Limita le azioni e le risorse di un'applicazione, proteggendo il sistema operativo attraverso regole definite per ogni programma.
+
+SUDO (Super User Do):
+"Sudo" è un comando nei sistemi operativi Linux che consente agli utenti privilegiati di eseguire specifici comandi come amministratori del sistema.
+
+Firewall e UFW:
+Un firewall monitora e controlla il traffico di rete, bloccando connessioni indesiderate. UFW è l'applicazione predefinita per gestire il firewall in sistemi operativi basati su Linux, semplificando la configurazione delle regole tramite riga di comando.
+
+SSH (Secure Shell):
+SSH è un protocollo crittografato per operare in modo sicuro su reti non sicure come Internet. Fornisce un canale sicuro per l'accesso remoto e la gestione di computer.
+
+CRON:
+Cron è un servizio di pianificazione del tempo per sistemi operativi Linux, consentendo l'esecuzione automatica di comandi o script a intervalli regolari attraverso cronjob.
